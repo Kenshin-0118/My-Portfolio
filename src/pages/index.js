fc71892224e77b08d4a518455865889fa43f6d9d
Kenshin-0118 <candia.0623.129476070013@gmail.com>
2024-05-26 10:10:59 +0800

    font

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,29 +61,29 @@ function Profile() {
       }
     }, [section]);
 
-    useEffect(() => {
-      // Disable right-click
-      document.addEventListener('contextmenu', (e) => e.preventDefault());
+    // useEffect(() => {
+    //   // Disable right-click
+    //   document.addEventListener('contextmenu', (e) => e.preventDefault());
   
-      // Disable certain keyboard shortcuts
-      document.addEventListener('keydown', (e) => {
-        if (
-          e.key === 'F12' || // Open DevTools
-          (e.ctrlKey && e.shiftKey && e.key === 'I') || // Ctrl+Shift+I
-          (e.ctrlKey && e.shiftKey && e.key === 'J') || // Ctrl+Shift+J
-          (e.ctrlKey && e.key === 'U') || // Ctrl+U (view source)
-          (e.ctrlKey && e.key === 'S') || // Ctrl+S (save page)
-          (e.ctrlKey && e.key === 'C') || // Ctrl+C (copy)
-          (e.ctrlKey && e.key === 'A') || // Ctrl+A (select all)
-          (e.ctrlKey && e.key === 'E') || // Ctrl+E (find)
-          (e.ctrlKey && e.key === 'H') || // Ctrl+H (find and replace)
-          (e.ctrlKey && e.key === 'F') || // Ctrl+F (find)
-          (e.ctrlKey && e.key === 'G') // Ctrl+G (find again)
-        ) {
-          e.preventDefault();
-        }
-      });
-    }, []);
+    //   // Disable certain keyboard shortcuts
+    //   document.addEventListener('keydown', (e) => {
+    //     if (
+    //       e.key === 'F12' || // Open DevTools
+    //       (e.ctrlKey && e.shiftKey && e.key === 'I') || // Ctrl+Shift+I
+    //       (e.ctrlKey && e.shiftKey && e.key === 'J') || // Ctrl+Shift+J
+    //       (e.ctrlKey && e.key === 'U') || // Ctrl+U (view source)
+    //       (e.ctrlKey && e.key === 'S') || // Ctrl+S (save page)
+    //       (e.ctrlKey && e.key === 'C') || // Ctrl+C (copy)
+    //       (e.ctrlKey && e.key === 'A') || // Ctrl+A (select all)
+    //       (e.ctrlKey && e.key === 'E') || // Ctrl+E (find)
+    //       (e.ctrlKey && e.key === 'H') || // Ctrl+H (find and replace)
+    //       (e.ctrlKey && e.key === 'F') || // Ctrl+F (find)
+    //       (e.ctrlKey && e.key === 'G') // Ctrl+G (find again)
+    //     ) {
+    //       e.preventDefault();
+    //     }
+    //   });
+    // }, []);
     
 
   return (
@@ -99,30 +99,30 @@ function Profile() {
 
     <div className='flex flex-col w-screen gap-4 relative mx-auto'>
       {/* Landing Page Section */}
-      <section className='w-full h-full relative p-4 md:p-20' data-aos="fade-up" data-aos-duration="500">
+      <section className='w-full h-full relative p-4 md:p-20' data-aos="fade-up" data-aos-once="true" data-aos-duration="500">
         <div className='w-full flex justify-center flex-col items-center backdrop-blur bg-opacity-75 p-4 md:p-14 rounded-xl overflow-hidden'>
           <div className='w-full h-full grid grid-cols-1 md:grid-cols-2 gap-2 lg:grid-cols-3'>
-          <div className='col-span-1 p-4 flex flex-col items-center justify-center' data-aos="fade-left" data-aos-delay="300">
+          <div className='col-span-1 p-4 flex flex-col items-center justify-center' data-aos="fade-right" data-aos-once="true" data-aos-delay="300">
               <div className='overflow-hidden rounded-full aspect-square bg-[#0d96d5] border-4 z-10 glow-effect'>
               <img src='ako.png' alt='my-photo' className='h-full w-full transition object-cover'/>
               </div>
             </div>
             <div className='col-span-1 lg:col-span-2 flex flex-col items-center justify-center p-4'>
               <span className='w-full text-[#0d96d5] font-bold text-xl sm:text-2xl lg:text-3xl text-left'
-              data-aos="fade-right" data-aos-delay="500">
+              data-aos="fade-right" data-aos-once="true" data-aos-delay="500" >
                 Hello!
               </span>
               <span className='w-full text-white font-bold text-2xl sm:text-4xl lg:text-6xl text-left indent-4'
-              data-aos="fade-right" data-aos-delay="650">
+              data-aos="fade-right" data-aos-once="true" data-aos-delay="650">
                 I'm Kenneth Candia
               </span>
               <span className='w-full text-gray-500 font-semibold text-lg sm:text-2xl lg:text-3xl text-left md:indent-8'
-              data-aos="fade-right" data-aos-delay="800">
+              data-aos="fade-right" data-aos-once="true" data-aos-delay="800">
                 Aspiring Full-Stack Developer
               </span>
               <span className='w-full text-white font-semibold text-lg md:text-xl text-right pt-10'
-              data-aos="fade-up" data-aos-delay="1000">
-                <button className='px-6 py-2 bg-[#0ea5e9] rounded-xl mr-2'onClick={() => window.open('https://docs.google.com/document/d/1SXXXnNHS_BbdXi-wLbUZffXLIJjP-_GCPVI-0XZbCWI/edit?usp=sharing', '_blank')}>
+              data-aos="fade-up" data-aos-once="true" data-aos-delay="1000">
+                <button className='px-6 py-2 bg-[#0ea5e9] rounded-xl mr-2 mb-2'onClick={() => window.open('https://docs.google.com/document/d/1SXXXnNHS_BbdXi-wLbUZffXLIJjP-_GCPVI-0XZbCWI/edit?usp=sharing', '_blank')}>
                   Show CV<FontAwesomeIcon icon={faFile} className='pl-2'/>
                 </button>
                 <button className='px-6 py-2 border-2 border-[#0ea5e9] rounded-xl' onClick={() => setSection('about')}>
@@ -138,19 +138,20 @@ function Profile() {
       <span className='about'/>
       <section className='w-full h-full relative p-4 md:p-8' data-aos="fade-up" data-aos-duration="500">
       <div className="w-full sticky top-16 z-50 text-white font-bold text-3xl  md:text-4xl py-4 bg-[#000c18]">About</div>
-      <div className='grid grid-cols-1 md:grid-cols-3  md:px-8 lg:px-16'>
-      <div className='col-span-1 md:col-span-2 h-full flex justify-center flex-col items-center p-4 rounded-xl overflow-hidden gap-4'>
+      <div className='grid grid-cols-1 lg:grid-cols-3  md:px-8 lg:px-16'>
+      <div className='col-span-1 lg:col-span-2 h-full flex justify-center flex-col items-center p-4 rounded-xl overflow-hidden gap-4'
+       data-aos="fade-right">
           <span className='w-full text-[#0ea5e9] font-semibold text-2xl sm:text-3xl lg:text-4xl text-left'>
             Hello, I am Kenneth I. Candia
           </span>
-          <span className='w-full text-slate-300 font-medium text-xl sm:text-2xl lg:text-3xl text-left indent-8'>
+          <span className='w-full text-slate-300 font-medium text-xl sm:text-2xl lg:text-3xl text-center lg:text-left md:indent-8'>
           A Graduating BSIT Student from the Holy Cross of Davao College, aspiring to be a proficient Full-Stack Developer. I have a strong foundation in front-end and back-end technologies, with hands-on experience in various programming languages and frameworks. Passionate about creating seamless web applications, I am eager to apply my skills to real-world projects, continuously learn new technologies, and contribute to innovative solutions that make a positive impact.
           </span>
       </div>
-      <div className='col-span-1 p-2'>
+      <div className='col-span-1 flex items-center p-2' data-aos="fade-left">
       <img src='programmer.gif' alt='my-photo' className='w-full transition squible glow-effect'/>
       </div>
-      <div className="col-span-1 md:col-span-2 lg:col-span-3 text-white font-bold text-3xl  md:text-4xl py-4 bg-[#000c18]">Tech Stack</div>
+      <div className="col-span-1 lg:col-span-3 text-white font-bold text-3xl  md:text-4xl py-4 bg-[#000c18]">Tech Stack</div>
       </div>
       </section>
 
